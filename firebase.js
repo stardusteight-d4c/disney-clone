7a906769aaf221f8484705845b7368e249e4e5d6
Gabriel Sena <stardusteight.d4cc@gmail.com>
2022-08-12 11:09:57 -0300

    fix env variable

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: "G-8GSGZQ44ST",
 };
 
